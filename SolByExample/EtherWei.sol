88d413eeae0d2d6aa157c951e0109d30d1d6a0a4
dev-ethar <tinayousofiali@gmail.com>
2022-11-10 02:59:14 -0800

    reading

---
+++ SolByExample/EtherWei.sol
@@ -0,0 +1,11 @@
+pragma solidity ^0.8.17;
+
+
+contract WeiAndEth {
+    uint public constant oneWei = 1;
+    // one wei
+    bool public constant isWeight = 1 wei == 1 ;
+    // 1 ether 2 * * 18 wei
+    bool public constant ethers = 1 ether == 1e18;
+
+}
