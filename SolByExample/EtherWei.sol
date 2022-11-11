1c696ca0d7573e6fc25f1fee1713d5bd07eac04e
dev-ethar <tinayousofiali@gmail.com>
2022-11-11 00:02:30 -0800

    pure and view functions undrestood

--- SolByExample/EtherWei.sol
+++ SolByExample/EtherWei.sol
@@ -1,5 +1,4 @@
-pragma solidity ^0.8.17;
-
+pragma solidity <=0.8.17;
 
 contract WeiAndEth {
     uint public constant oneWei = 1;
@@ -7,5 +6,4 @@ contract WeiAndEth {
     bool public constant isWeight = 1 wei == 1 ;
     // 1 ether 2 * * 18 wei
     bool public constant ethers = 1 ether == 1e18;
-
 }
