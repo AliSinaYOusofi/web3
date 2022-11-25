a2c3b341e6c9af6835d2df183216687909945113
dev-ethar <tinayousofiali@gmail.com>
2022-11-25 00:02:29 -0800

    serious

---
+++ block-exp/app/Table/page.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Inputs from '../../component/Inputs';
+import Table from '../../component/Table';
+
+export default function Table() {
+    return ( 
+        <>
+            <Inputs />
+            <Table />
+        </>
+    );
+}
---
+++ block-exp/app/page.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function page() {
+  return (
+    <div className="text-2xl">
+        page
+        <p className="text-blue-400">fuck</p>
+    </div>
+  )
+}
