a97b0f3dc240aab7e6da45093f7922a4e1fddbd9
dev-ethar <tinayousofiali@gmail.com>
2022-11-25 09:34:16 -0800

    block exp done

--- block-exp/pages/api/info.js
+++ block-exp/pages/api/info.js
@@ -1,10 +1,11 @@
 import Web3 from 'web3';
 
-export async function handler(req, res) {
+export default async function handler(req, res) {
     // getting the value from the thing
     // TODO1: get value from front-end
     // TODO2: scan the blockchain for all the number.
     // TODO3: send back the result and display it on the front-end.
+    
     const web3 = new Web3("https://celo-mainnet.infura.io/v3/284c8a579bef4db28153c33f9656010e");
 
     const {block} = req.body; // the counter
@@ -13,7 +14,7 @@ export async function handler(req, res) {
 
     let counter = 0;
     let blockInfoData = [];
-    
+
     for (counter; counter < Number(block); counter++) {
         
         let currentBlock = await web3.eth.getBlock(latestBlock - counter)
@@ -31,4 +32,5 @@ export async function handler(req, res) {
         }
     }
 
+    res.status(200).json({data: blockInfoData})
 }
